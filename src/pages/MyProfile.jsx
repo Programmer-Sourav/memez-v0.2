30419ea4f9cdcfb991dadd77ab45c01fe78f58bd
Programmer-Sourav <35804676+Programmer-Sourav@users.noreply.github.com>
2023-07-01 18:18:49 +0530

    feat: logout
    logout

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -3,17 +3,29 @@ import { ApplicationContext } from "../context/ApplicationContext"
 import { EditModal } from "../components/EditModal"
 import { ChakraProvider } from "@chakra-ui/react"
 import { useEffect } from "react"
+import { AUTH_ACTION_TYPE } from "../auth/ActionType"
+import { useNavigate } from "react-router"
 
 export default function MyProfile() {
     
-    const { posts, homePageDispatch, authenticatedUser, editProfile, updatedUser, profileImage, setProfileImage, images, updateImages, onChnageBio, onChangeUrl, imagedata} = useContext(ApplicationContext)
+    const { posts, homePageDispatch, authenticatedUser, editProfile, updatedUser, profileImage, setProfileImage, images, updateImages, onChnageBio, onChangeUrl, imagedata, users, authDispatch} = useContext(ApplicationContext)
 
-    
+    const navigate = useNavigate()
+
     const getData = () =>{
       const findUrl = images.find((imageItem)=>imageItem.id===5) 
       setProfileImage({...profileImage, image: findUrl.image})
     }
+    const userAvatar = (username) =>{
+      const user = users.find((user)=>user.username===username)
+      console.log("User ", user.avatar)
+      return user.avatar
+    }
     
+    const onCallingLogout= () =>{
+      authDispatch({type: AUTH_ACTION_TYPE.ACTION_LOG_OUT, payload: {isLoggedIn: false, authenticatedUser: null}})
+      navigate("/")
+    }
     useEffect(()=>{getData()},[])
     
     return (
@@ -22,7 +34,8 @@ export default function MyProfile() {
         <div class="flex flex-column flex-center">
           <div class="lynx-gray-bg width-7 height-7 br-full"><img src={profileImage.image} alt="dummyavatar" style={{borderRadius: "50%"}}></img></div>
           <h3 class="pt-s">@{authenticatedUser.username}</h3>
-          <p class="grey-color txt-s">@{authenticatedUser.firstName}</p>
+          <p class="grey-color txt-s">@{authenticatedUser.firstName}</p> 
+          <button style={{padding: "8px", margin: "8px", background: "red", borderRadius: "8px", color:"white"}} onClick={onCallingLogout}  >Logout</button>
           <button class="border lynx-white-bg p-xs m-xs fw-semibold width-8">
            <ChakraProvider><EditModal data={authenticatedUser}/></ChakraProvider> 
           </button>
@@ -51,7 +64,7 @@ export default function MyProfile() {
             { posts.filter((postItem)=>postItem.username===authenticatedUser.username)
             .map((postItem)=>(
           <div class="flex flex-row nowrap p-xs">
-            <div class="grey-bg br-full width-xl height-xl p-xs mr-xs"></div>
+            <div class="grey-bg br-full width-xl height-xl p-xs mr-xs">{<img src={userAvatar(authenticatedUser.username)}  alt="dummy" style={{borderRadius: "90%"}}/>}</div>
             <div>
               <div class="flex flex-row flex-align-center flex-space-between">
                 <div class="flex flex-row">
