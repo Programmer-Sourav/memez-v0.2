642bac74ec1d7f1236ca9ea437287112eb540e65
Programmer-Sourav <35804676+Programmer-Sourav@users.noreply.github.com>
2023-07-03 10:20:55 +0530

    fix: post desc
    Post desc fix

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,11 +1,14 @@
 import { useContext } from "react"
 import { ApplicationContext } from "../context/ApplicationContext"
 import { useParams } from "react-router-dom"
+import { useState } from "react"
 
 export default function PostPage({posts}){
 
-    const { authenticatedUser } = useContext(ApplicationContext)
+    const { authenticatedUser, users } = useContext(ApplicationContext)
     const { _id } = useParams(ApplicationContext)
+    const [comment, setComment] = useState("")
+    const [show, setShow] = useState(false)
 
     const findThePost =  getThisPost()
 
@@ -13,6 +16,23 @@ export default function PostPage({posts}){
         const result = posts.find((post)=>post._id===_id)
         console.log(1234, result)
         return result
+    }
+
+    const findUser = getTheUser()
+
+    function getTheUser(){
+
+        const result = users.find((user)=>user.username===findThePost.username)
+        return result
+    }
+
+    const addAComment = () =>{
+     setShow(true) 
+    }
+
+    const onChangeHandler = (e) =>{
+      setComment(e.target.value)
+      console.log(e.target.value)
     }
 
 return(
@@ -24,11 +44,11 @@ return(
         <div class="white-bg p-xs">
           <div class="flex flex-column nowrap p-xs">
             <div class="flex">
-              <div class="grey-bg br-full width-xl height-xl p-xs mr-xs" style={{aspectRatio: "1"}}><img src={authenticatedUser.avatar} alt="profilePic" style={{borderRadius:"50%"}}/></div>
+              <div class="grey-bg br-full width-xl height-xl p-xs mr-xs" style={{aspectRatio: "1"}}><img src={findUser.avatar} alt="profilePic" style={{borderRadius:"50%"}}/></div>
               <div class="flex flex-row flex-align-center flex-space-between w-full">
                 <div class="flex flex-column">
-                  <p class="fw-semibold">{authenticatedUser.username}</p>
-                  <p class="grey-color">@{authenticatedUser.username}</p>
+                  <p class="fw-semibold">{findUser.username}</p>
+                  <p class="grey-color">@{findUser.username}</p>
                 </div>
                 <p class="">∙∙∙</p>
               </div>
@@ -55,36 +75,31 @@ return(
         </div>
         <div class="w-full" style={{height: "1px", backgroundColor: "rgb(206, 206, 206)"}}></div>
         <div class="flex p-s">
-          <div class="grey-bg br-full width-xl height-xl p-xs mr-s" style={{aspectRatio: "1"}}></div>
-        <input type="text" class="lynx-gray-bg border-none outline-transparent w-full mr-xs p-xs " style={{backgroundColor: "rgb(242, 242, 242)"}} placeholder="Comment your reply"/>
-        <button class="primary-bg secondary-color border-none outline-transparent pr-xl pl-xl">Post</button>
+          <div class="grey-bg br-full width-xl height-xl p-xs mr-s" style={{aspectRatio: "1"}}>{<img src={authenticatedUser.avatar} alt="profilepic" style={{borderRadius: "50%"}}/>}</div>
+        <input type="text" class="lynx-gray-bg border-none outline-transparent w-full mr-xs p-xs " value={comment} style={{backgroundColor: "rgb(242, 242, 242)"}} placeholder="Comment your reply" onChange={(e)=>{onChangeHandler(e)}}/>
+        <button class="primary-bg secondary-color border-none outline-transparent pr-xl pl-xl" onClick={()=>{addAComment()}}>Post</button>
         </div>
         <div class="w-full" style={{height: "1px", backgroundColor: "rgb(206, 206, 206)"}}></div>
-        <div class="white-bg  p-xs mt-s">
+        {console.log(comment, show)}
+       {comment && show ?( <div class="white-bg  p-xs mt-s">
           <div class="flex flex-row nowrap p-xs w-full">
-            <div class="grey-bg br-full width-xl height-xl p-xs mr-xs" style={{aspectRatio: "1"}}></div>
+            <div class="grey-bg br-full width-xl height-xl p-xs mr-xs" style={{aspectRatio: "1"}}>{<img src={authenticatedUser.avatar} alt="profilepic" style={{borderRadius: "50%"}}/>}</div>
             <div>
               <div class="flex flex-row flex-align-center  flex-space-between">
                 <div class="flex flex-column">
                   <div class="flex flex-row">
-                    <p class="fw-bold">Ashwin Khode</p>
+                    <p class="fw-bold">@{authenticatedUser.firstName}</p>
                     <p class="grey-color pl-xs">
-                      @ashwin4real <span class="pl-xs">•</span>
+                      @{authenticatedUser.username} <span class="pl-xs">•</span>
                       <span class="pl-xs">1m</span>
                     </p></div>
                   <p></p>
-                  <p class="lynx-gray-color">Replying to <span class="primary-color">@tanaypratap</span></p>
+                  <p class="lynx-gray-color">Replying to <span class="primary-color">@{findUser.username}</span></p>
                 </div>
                 <p class="">∙∙∙</p>
               </div>
               <p class="pr-s pt-xs">
-                Non programmers on my timeline. Attention. 
-              
-                After placing 100+ programmers i in top Indian startups, I am
-                thinking of coming up with a program for business roles as well.
-                
-                Interested in helping me build this course? Join the telegram
-                group (in next tweet)
+                {comment}
               </p>
               <div class="flex flex-row nowrap flex-space-between pb-xs pt-m pr-s flex-align-center">
                 <i class="bi bi-heart"></i>
@@ -94,7 +109,7 @@ return(
               </div>
             </div>
           </div>
-        </div>
+        </div>) : ""}
       </main>
     </div>
 )
